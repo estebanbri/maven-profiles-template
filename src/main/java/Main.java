a46f183efe36764a29536b88cf1468e2f15a0433
Esteban <esteban_89_1@hotmail.com>
2018-12-09 19:15:52 +0100

    Maven Profile Template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
-import org.apache.maven.settings.Settings;
-import java.util.List;
-
+import java.util.ResourceBundle;
 
 public class Main {
-
+    //Accedo al archivo environment.properties
+    static ResourceBundle propretiesFileGeneral = ResourceBundle.getBundle("environment");
 
     public static void main(String[] args) {
-        Settings settings = new Settings();
-        List<String> perfilesActivos = settings.getActiveProfiles();
-        System.out.println(perfilesActivos);
+        //Accedo y obtengo el valor de la key 'ambiente_activo_key' del archivo environment.properties
+        String ambienteActivoValue = propretiesFileGeneral.getString("ambiente_activo_key");
 
+        //Accedo al archivo .properties segun el valor que se haya almacenado en la variable anterior ambienteActivoValue
+        ResourceBundle propertiesFileSegunProfileActivo = ResourceBundle.getBundle(ambienteActivoValue);
+
+        //Accedo a el valor de la key conexion_database_key del archivo properties segun el profile activo
+        String conexionDatabaseValue = propertiesFileSegunProfileActivo.getString("conexion_database_key");
+
+        System.out.println(conexionDatabaseValue);
 
     }
 }
